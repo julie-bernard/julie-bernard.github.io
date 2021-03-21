1cce408f4a8962a9065e3d96eb11d417e9e9bae5
Simon BERNARD <simon.bernard@polyconseil.fr>
2021-03-21 20:19:01 +0100

    deploy

--- _next/static/vXbAHBqbBZczs2HEAC6uB/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-ca37bb6d00725f45be16.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-501b05b27a9d8750923b.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
